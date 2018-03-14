5f9fc5037d1c79ff5ad74d4f93b9b28c7010b596
Mehedi Abdullah <aamehedi93@gmail.com>
2018-03-14 21:36:29 +0600

    Shell script for bash atom config
    Shell script for installing || config atom packages.

---
+++ atom.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+apm install atom-beautify
+apm install minimap
+apm install linter-ui-default
+apm install file-icons
+apm install linter
+apm install pigments
+apm install git-plus
+apm install language-babel
+apm install emmet
+apm install color-picker
+apm install busy-signal
+apm install intentions
+apm install highlight-selected
+apm install platformio-ide-terminal
+apm install autoclose-html
+apm install atom-ide-ui
+apm install minimap-highlight-selected
+apm install atom-clock
+apm install bracket-matcher
+apm install language-shellscript
+apm install autocomplete-css
+apm install atom-runner
+apm install highlight-line
+
+ 
